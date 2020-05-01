bd1d83ad7c5949fecaacd8a898d328be70ebc9ed
bogodars <bogodar@gmail.com>
2020-05-01 22:16:40 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $(".header__slider").slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
+    });
+
+
+    $(".slider-dots").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: ".header__slider",
+        infinite: false,
+    });
+
+    $(".surf-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: ".slider-map",
+        infinite: true,
+    });
+
+    $(".slider-map").slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: ".surf-slider",
+        focusOnSelect: true,
+        infinite: false,
+    });
+});
